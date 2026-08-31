8f168eb6b78659ed4d78816d77131ad51389c509
Ebrahimim29 <Ebrahimim29@gmail.com>
2026-08-31 07:38:07 +0330

    HTTP Methods in API Routes

--- pages/api/hello.jsx
+++ pages/api/hello.jsx
@@ -1,3 +1,10 @@
 export default function handler(req, res) {
+    
+    console.log(req.method);
+
+    if(req.method != "POST"){
+        return res.status(405).json("Method Not Allowed");
+    }
+   
     res.status(200).json({ name: "Mohammad MirEbrahimi"});
 }
